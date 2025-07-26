0fda4063eb7446974b548dd885fc4a214a7c58a6
sushantthapa <sus.hant@outlook.com>
2025-07-27 03:45:15 +1000

    Updates path to hello.py from day3/test_hello.py

--- day3/test_hello.py
+++ day3/test_hello.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def test_hello_script_runs():
-    result = subprocess.run(["python", "day1/hello.py"], capture_output=True, text=True)
+    result = subprocess.run(["python", "../day1/hello.py"], capture_output=True, text=True)
     assert result.returncode == 0
     assert "Hello" in result.stdout

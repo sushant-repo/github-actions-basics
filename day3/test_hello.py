7f08964ce65a2499d196d1e10b9253d743315420
sushantthapa <sus.hant@outlook.com>
2025-07-27 03:41:01 +1000

    trying out chaining linting, testing, build package and deployment

---
+++ day3/test_hello.py
@@ -0,0 +1,6 @@
+import subprocess
+
+def test_hello_script_runs():
+    result = subprocess.run(["python", "day1/hello.py"], capture_output=True, text=True)
+    assert result.returncode == 0
+    assert "Hello" in result.stdout
